a7c64d6a31f723b4d6c8032280dabaf206eee3bc
ulum <apumm12@gmail.com>
2025-02-21 18:26:14 +0700

    selesai mteri string

--- 05.string/index.js
+++ 05.string/index.js
@@ -17,38 +17,41 @@ console.log(str.toUpperCase()); // merubah string ke uppercase
 
 console.log(str.toLowerCase()); // merubah string ke lowecase
 
-let testTrim = "       javascript       trim     ";
+let testTrim = "       javascript trim     "; // Menghilangkan spasi di awal dan di akhir jika ada banyak spasi di tengah tidak akan di hilangkan
 
 console.log(testTrim.trim());
 
 // Manipulasi String
 
 // concat
-let firstName = "Ranggo";
-let lastName = "Pato";
+let firstName = "Ulum";
+let lastName = "Dcode";
 let result = firstName + " " + lastName;
 console.log(result);
 
 // // concat template literals
 
-let resultBactics = `${firstName} ${lastName}`;
-console.log(resultBactics);
+let resultBacktics = `${firstName} ${lastName}`;
+console.log(resultBacktics);
 
 // // Mengambil bagian dari string
 
 let text = "JavaScript";
 console.log(text.slice(0, 4));
 console.log(text.substring(4));
+// Melihats character berdasarkan index
+console.log(str.at(0));
+console.log(str.charAt(1));
 
 // // Mengganti bagian dari string
-// let replacement = 'Hello, World'
-// let newReplacement = replacement.replace('World', 'There')
+let replacement = "Hello, World";
+let newReplacement = replacement.replace("World", "There");
 
 console.log(newReplacement);
 
 const arr = ["test", "array", "di", "string"];
 
-// console.log(arr.join('-'))
+console.log(arr.join(" "));
 
 const testString = "test-array-di-string";
 
@@ -64,5 +67,5 @@ console.log(index);
 let lastIndex = sentence.lastIndexOf("dolor");
 console.log(lastIndex);
 
-let exists = sentence.includes("dolorisme");
+let exists = sentence.includes("dolor");
 console.log(exists);
